3d9a8ba87224580b1e4961bd4029b69b6b42cbb8
cosmopoliitans <elfkyn52@gmail.com>
2023-03-30 19:21:29 +0300

    Kahveler

---
+++ src/CoffeeList.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function CoffeeList() {
+  const [coffeeData, setCoffeeData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://api.sampleapis.com/coffee/hot")
+      .then((response) => {
+        setCoffeeData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <div className="container">
+        <ul>
+          {coffeeData.map((coffee) => (
+            <li key={coffee.id}>
+              <img src={coffee.image} alt={coffee.title} />
+              <h3>{coffee.title}</h3>
+              <p>
+                {coffee.ingredients.map((ingredient, index) => (
+                  <React.Fragment key={index}>
+                    {index > 0 && ", "}
+                    {ingredient}
+                  </React.Fragment>
+                ))}
+              </p>
+              <p>{coffee.description}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default CoffeeList;
